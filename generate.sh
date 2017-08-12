ed890a5bd7714a95cfb6f79c8dc077b6126baeca
Qingshan Zhuan <zhuanqingshan@gmail.com>
2017-08-13 00:16:01 +1000

    lean vs agile vs design thinking

--- generate.sh
+++ generate.sh
@@ -3,9 +3,11 @@ bookname=$1
 authorname=$2
 
 pushd _posts
-sed -i '' 's/%bookname%/'$bookname'/g' template.md 
-sed -i '' 's/%author%/'$authorname'/g' template.md 
+sed -i '' "s/%bookname%/$bookname/g" template.md 
+sed -i '' "s/%author%/$authorname/g" template.md 
 
 date=`date "+%Y-%m-%d"`
-mv template.md $date'-读《'$bookname'》.md'
+newName=$date'-读《'$bookname'》.md'
+echo newName
+mv template.md "$newName"
 popd
